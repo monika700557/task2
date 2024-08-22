16193f489fbe24da30780f01f2331f1c38f4445c
monika700557 <monika.tiarasoftware@gmail.com>
2024-08-22 11:44:35 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,122 @@
+
+
+const express = require('express');
+const fs = require('fs').promises;
+const handlebars = require('handlebars');
+const path = require('path');
+const puppeteer = require('puppeteer');
+const app = express();
+const port = 4001;
+
+const finalHtmlPath = path.join(__dirname, 'finalTemplate.html');
+
+const { data } = require('./mockData');
+
+const metal = data.sell_info.cart[0].product_details.Metal;
+
+function cleanData(data) {
+    const fieldsToRemove = [
+        'product_id'
+        // 'po_product_id', 'Metal', 'stone_group', 'stone', 'shape', 'Cut',
+        // 'quality', 'clarity', 'color', 'Size', 'Unit', 'setting_type', 'sessioncartid',
+        // 'id', 'payment_method_id', 'customer_id', 'sales_person_id', 'location_id',
+        // 'main_image', 'custom_cart_id', 'po_product_Id', 'UDID',
+    ];
+
+    function removeFields(item) {
+        if (Array.isArray(item)) {
+            return item.map(removeFields);
+        } else if (item && typeof item === 'object') {
+            return Object.entries(item).reduce((acc, [key, value]) => {
+                if (!fieldsToRemove.includes(key)) {
+                    acc[key] = removeFields(value);
+                }
+                return acc;
+            }, {});
+        }
+        return item;
+    }
+
+    return removeFields(data);
+}
+
+app.use(express.static(__dirname));
+
+
+app.get('/', async (req, res) => {
+    try {
+
+        const cleanedData = cleanData(data);
+        const templatePath = path.join(__dirname, 'template.html');
+        const headerPath = path.join(__dirname, 'partials', 'header.html');
+        const footerPath = path.join(__dirname, 'partials', 'footer.html');
+        const cssPath = path.join(__dirname, 'styles.css'); // Path to your CSS file in the root directory
+
+        const templateHtml = await fs.readFile(templatePath, 'utf8');
+        const headerHtml = await fs.readFile(headerPath, 'utf8');
+        const footerHtml = await fs.readFile(footerPath, 'utf8');
+        const cssContent = await fs.readFile(cssPath, 'utf8'); // Read the CSS content
+
+        handlebars.registerPartial('header', headerHtml);
+        handlebars.registerPartial('footer', footerHtml);
+        const template = handlebars.compile(templateHtml);
+
+
+        const finalHtml = template({ ...cleanedData });
+        const htmlWithCss = `
+            <!DOCTYPE html>
+            <html lang="en">
+            <head>
+                <meta charset="UTF-8">
+                <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                <style>${cssContent}</style>
+            </head>
+            <body>
+                ${finalHtml}
+            </body>
+            </html>
+        `;
+
+        await fs.writeFile(finalHtmlPath, htmlWithCss, 'utf8');
+
+        res.send(htmlWithCss);
+    } catch (error) {
+        console.error('Error:', error);
+        res.status(500).send('An error occurred');
+    }
+});
+
+
+app.get('/api/cleaned-data', async (req, res) => {
+    try {
+        const cleanedData = cleanData(data);
+        res.json(cleanedData);
+    } catch (error) {
+        console.error('Error:', error);
+        res.status(500).send('An error occurred');
+    }
+});
+
+app.get('/generate-pdf', async (req, res) => {
+    try {
+
+        const html = await fs.readFile(finalHtmlPath, 'utf8');
+
+        const browser = await puppeteer.launch({ headless: true, args: ['--no-sandbox', '--disable-setuid-sandbox'] });
+        const page = await browser.newPage();
+        await page.setContent(html, { waitUntil: 'networkidle0' });
+        const pdfPath = path.join(__dirname, 'output.pdf');
+        console.log('PDF will be saved to:', pdfPath);
+        await page.pdf({ path: pdfPath, format: 'A4' ,  printBackground: true   });
+        await browser.close();
+
+        res.send(`PDF generated and saved to ${pdfPath}.`);
+    } catch (error) {
+        console.error('Error during PDF generation:', error);
+        res.status(500).send('An error occurred while generating the PDF.');
+    }
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+});
---
+++ mockData.js
@@ -0,0 +1,357 @@
+const data = {
+  "sell_info": {
+    "cart": [
+      {
+        
+        "product_details": {
+          "product_id": "6568539997b0da5e821c4019",
+          "po_product_id": "65a08e43f3ff48b82205b902",
+          "Metal": "6355fbddc734e545a850dd02",
+          "Stones": [
+            {
+              "stone_group": "63560539dcf7ec0c616fea1f",
+              "stone": "6356061d2572146aa404d2d2",
+              "Variant": "2",
+              "Align": "",
+              "shape": "63560779c0d74d7f332ea992",
+              "Cut": "635607932572146aa404d2d6",
+              "quality": "",
+              "clarity": "636c7b71671a877a9c779c81",
+              "color": "636c7c4a28021528d413c0b9",
+              "Size": "63560917044ba54bde4d2a16",
+              "Unit": "Cts",
+              "Pcs": "1",
+              "Cts": "0.20",
+              "Price": "20000",
+              "Pointer": "0.20",
+              "setting_type": "636c8418bc8a8f3720133d64",
+              "certificate": {
+                "type": "",
+          
+                "number": ""
+              },
+              "size_name": "3.80MM",
+              "stone_name": "Diamond",
+              "shape_name": "Brilliant Round",
+              "clarity_name": "VS2",
+              "color_name": "J",
+              "setting_name": "Solitaire",
+              "cut_name": "Heart & Arrow"
+            },
+            {
+              "stone_group": "63560539dcf7ec0c616fea1f",
+              "stone": "6356061d2572146aa404d2d2",
+              "Variant": "0",
+              "Align": "",
+              "shape": "63560779c0d74d7f332ea992",
+              "Cut": "6375cf6b3c04c604447519ea",
+              "quality": "",
+              "clarity": "636c7b71671a877a9c779c81",
+              "color": "636c7c4a28021528d413c0b9",
+              "Size": "63560917044ba54bde4d2a16",
+              "Unit": "Cts",
+              "Pcs": "100",
+              "Cts": "0.20",
+              "Price": "0.00",
+              "Pointer": "0",
+              "setting_type": "635648d4c0d74d7f332ea994",
+              "certificate": {
+                "type": "",
+                "number": ""
+              },
+              "size_name": "3.80MM",
+              "stone_name": "Diamond",
+              "shape_name": "Brilliant Round",
+              "clarity_name": "VS2",
+              "color_name": "J",
+              "setting_name": "Pave",
+              "cut_name": "Normal Cut"
+            },
+            {
+              "stone_group": "63560539dcf7ec0c616fea1f",
+              "stone": "6356061d2572146aa404d2d2",
+              "Variant": "0",
+              "Align": "",
+              "shape": "63560779c0d74d7f332ea992",
+              "Cut": "6375cf6b3c04c604447519ea",
+              "quality": "",
+              "clarity": "636c7b8928021528d413c0b7",
+              "color": "636c7c4a28021528d413c0b9",
+              "Size": "63858e2ec0d74d7f332eaa15",
+              "Unit": "Cts",
+              "Pcs": "2",
+              "Cts": "0.20",
+              "Price": "0.00",
+              "Pointer": "0",
+              "setting_type": "635648d4c0d74d7f332ea994",
+              "certificate": {
+                "type": "",
+                "number": ""
+              },
+              "size_name": "1.25MM",
+              "stone_name": "Diamond",
+              "shape_name": "Brilliant Round",
+              "clarity_name": "VVS2",
+              "color_name": "J",
+              "setting_name": "Pave",
+              "cut_name": "Normal Cut"
+            }
+          ],
+          "product_variants_enable": "2",
+          "stock_id": "2211761",
+          "po_QTY": 1,
+          "retailprice_Inc": 31800
+        },
+        "customer_id": "6379ae40a14336b22805540c",
+        "sales_person_id": "6379ae9e28021528d413c0df",
+        "location_id": "634f6d59ae461466b73d0959",
+        "Qty": 1,
+        "type": "pos_order",
+        "product_type": "inventory",
+        "sessioncartid": "961aa3d1-4ca1-4f98-9218-2a995bde6490",
+        "status": {
+          "is_custom_design": 0,
+          "cart_status": 0,
+          "priority": 0
+        },
+        "retailprice_Inc": 31800,
+        "price": 31800,
+        "order_type": "pos_order",
+        "SKU": "SB03218KWGDM-44",
+        "po_QTY": 0,
+        "qtyToBe": 0,
+        "main_image": "https://gis247.s3.us-east-2.amazonaws.com/2022th0038/products/image/SB03218KWGDM-41.png",
+        "product_id": "6568539997b0da5e821c4019",
+        "po_product_id": "65a08e43f3ff48b82205b902",
+        "name": "C-CLASSIC PAVE BAND SOLITAIRE COLLECTION",
+        "Metal": "6355fbddc734e545a850dd02",
+        "Size": "636c6e94c734e545a850dd4b",
+        "Stones": [
+          {
+            "stone_group": "63560539dcf7ec0c616fea1f",
+            "stone": "6356061d2572146aa404d2d2",
+            "Variant": "2",
+            "Align": "",
+            "shape": "63560779c0d74d7f332ea992",
+            "Cut": "635607932572146aa404d2d6",
+            "quality": "",
+            "clarity": "636c7b71671a877a9c779c81",
+            "color": "636c7c4a28021528d413c0b9",
+            "Size": "63560917044ba54bde4d2a16",
+            "Unit": "Cts",
+            "Pcs": "1",
+            "Cts": "0.20",
+            "Price": "20000",
+            "Pointer": "0.20",
+            "setting_type": "636c8418bc8a8f3720133d64",
+            "certificate": {
+              "type": "",
+              "number": ""
+            },
+            "size_name": "3.80MM",
+            "stone_name": "Diamond",
+            "shape_name": "Brilliant Round",
+            "clarity_name": "VS2",
+            "color_name": "J",
+            "setting_name": "Solitaire",
+            "cut_name": "Heart & Arrow"
+          },
+          {
+            "stone_group": "63560539dcf7ec0c616fea1f",
+            "stone": "6356061d2572146aa404d2d2",
+            "Variant": "0",
+            "Align": "",
+            "shape": "63560779c0d74d7f332ea992",
+            "Cut": "6375cf6b3c04c604447519ea",
+            "quality": "",
+            "clarity": "636c7b71671a877a9c779c81",
+            "color": "636c7c4a28021528d413c0b9",
+            "Size": "63560917044ba54bde4d2a16",
+            "Unit": "Cts",
+            "Pcs": "100",
+            "Cts": "0.20",
+            "Price": "0.00",
+            "Pointer": "0",
+            "setting_type": "635648d4c0d74d7f332ea994",
+            "certificate": {
+              "type": "",
+              "number": ""
+            },
+            "size_name": "3.80MM",
+            "stone_name": "Diamond",
+            "shape_name": "Brilliant Round",
+            "clarity_name": "VS2",
+            "color_name": "J",
+            "setting_name": "Pave",
+            "cut_name": "Normal Cut"
+          },
+          {
+            "stone_group": "63560539dcf7ec0c616fea1f",
+            "stone": "6356061d2572146aa404d2d2",
+            "Variant": "0",
+            "Align": "",
+            "shape": "63560779c0d74d7f332ea992",
+            "Cut": "6375cf6b3c04c604447519ea",
+            "quality": "",
+            "clarity": "636c7b8928021528d413c0b7",
+            "color": "636c7c4a28021528d413c0b9",
+            "Size": "63858e2ec0d74d7f332eaa15",
+            "Unit": "Cts",
+            "Pcs": "2",
+            "Cts": "0.20",
+            "Price": "0.00",
+            "Pointer": "0",
+            "setting_type": "635648d4c0d74d7f332ea994",
+            "certificate": {
+              "type": "",
+              "number": ""
+            },
+            "size_name": "1.25MM",
+            "stone_name": "Diamond",
+            "shape_name": "Brilliant Round",
+            "clarity_name": "VVS2",
+            "color_name": "J",
+            "setting_name": "Pave",
+            "cut_name": "Normal Cut"
+          }
+        ],
+        "product_variants_enable": "2",
+        "stock_id": "2211761",
+        "size_name": "44",
+        "delivery_date": "2024-10-12T07:27:03.873Z",
+        "custom_cart_id": "66b716478690a3649315a959",
+        "po_product_Id": "65a08e43f3ff48b82205b902",
+        "stock_id_options": [
+          { "value": "2211791", "label": "2211791", "po_QTY": 1 },
+          { "value": "2211792", "label": "2211792", "po_QTY": 1 },
+          { "value": "2211793", "label": "2211793", "po_QTY": 1 },
+          { "value": "2211755", "label": "2211755", "po_QTY": 1 },
+          { "value": "2211757", "label": "2211757", "po_QTY": 1 },
+          { "value": "2211760", "label": "2211760", "po_QTY": 1 },
+          { "value": "2211756", "label": "2211756", "po_QTY": 1 },
+          { "value": "2211758", "label": "2211758", "po_QTY": 1 },
+          { "value": "2211759", "label": "2211759", "po_QTY": 1 },
+          { "value": "220712", "label": "220712", "po_QTY": 1 },
+          { "value": "220710", "label": "220710", "po_QTY": 1 },
+          { "value": "220815", "label": "220815", "po_QTY": 1 },
+          { "value": "220627", "label": "220627", "po_QTY": 1 },
+          { "value": "220565", "label": "220565", "po_QTY": 1 },
+          { "value": "220572", "label": "220572", "po_QTY": 1 },
+          { "value": "220599", "label": "220599", "po_QTY": 1 },
+          { "value": "220604", "label": "220604", "po_QTY": 1 },
+          { "value": "220653", "label": "220653", "po_QTY": 1 },
+          { "value": "220654", "label": "220654", "po_QTY": 1 }
+        ],
+        "status_type": "pos_order",
+        "discount_percent": 0,
+        "Discount_Amount": 0,
+        "stone_name": "Diamond",
+        "Service_labour_exsist": false,
+        "Service_labour_total_charges": 0,
+        "id": "6568539997b0da5e821c4019"
+      }
+    ],
+    "deletecart_ids": [],
+    "summary_order": {
+      "labour": 0,
+      "shipping": 0,
+      "loyality_points": 0,
+      "customer_id": {
+        "id": "6379ae40a14336b22805540c",
+        "name": "Scarlett J Johansson",
+        "existsInAddress": true
+      },
+      "sales_person_id": {
+        "value": "6379ae9e28021528d413c0df",
+        "label": "Born Born"
+      },
+      "discount": 0,
+      "discount_percent": 0,
+      "deposit": 0,
+      "deposit_amount": 0,
+      "tax_amount": "2080.37",
+      "tax_amount_int": 2081,
+      "tax_prect": 7,
+      "tax_type": "Inclusive",
+      "Sub_Total": 29719
+    },
+    "status_type": "pos_order",
+    "totalamount": 31800,
+    "park_id": "",
+    "order_id": "",
+    "service_labour": []
+  },
+  "payment_info": {
+    "debited_amount": {
+      "cash": 31800,
+      "bank": 0,
+      "credit_card": 0,
+      "credit_notes": 0
+    },
+    "pay_data": {
+      "cash": {
+        "payment_method_id": "634f6d59ae461466b73d095a",
+        "Paymentmethod_type": "cash",
+        "amount": 31800
+      },
+      "bank": [],
+      "IB": [],
+      "credit_card": [],
+      "credit_note": [],
+      "gift_card": ""
+    },
+    "balance_due": 0,
+    "balance_deposit": 31800
+  },
+
+  "transaction_date": "2024-08-10T07:27:10.224Z",
+  "layby_due_date": "",
+  "pay_layby_data": [],
+  "paymentMathod": "Full_Payment",
+  "installment": [],
+  "fullinstallentlist": [],
+  "customer_id": "6379ae40a14336b22805540c",
+  "sales_person_id": "6379ae9e28021528d413c0df",
+  "order_type": "pos_order",
+  "order_id": "",
+  "quatation_id": "",
+  "layby_id": "",
+  "by_custom_order": "",
+  "custom_order_id": "",
+  "by_repair_order": "",
+  "reserve_sell": "",
+  "service_enable": false,
+  "shippingInfo": {
+    "billing_address": {
+      "country": {
+        "label": "United States",
+        "value": "231"
+      },
+      
+    
+      "city": "New York",
+      "state": "NY",
+      "address": "1134 Lexington Ave",
+      "UDID": "661693adda43b3024d52358a60",
+      "is_default": 1,
+      "tax_number": "TIN0989723",
+      "zipcode": "10075",
+      "fullAddress": "1134 Lexington Ave, New York, NY, United States, 10075"
+    },
+    "shipping_address": {
+      "tax_number": "TXNIUD92028134",
+      "address": "524 Roxboro PI NW",
+      "city": "DC",
+      "UDID": "661693adda43b3024d52358a60",
+      "is_default": 1,
+      "state": "Washington",
+      "country": {
+        "label": "United States",
+        "value": "231"
+      },
+      "zipcode": "20011",
+      "fullAddress": "524 Roxboro PI NW, DC, Washington, United States, 20011"
+    }
+  }
+};
+
