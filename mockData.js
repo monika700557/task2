8452d2302c6c8bf01a3b1812f08f2d6a135179f0
monika700557 <monika.tiarasoftware@gmail.com>
2024-08-23 18:38:28 +0530

    Update mockData.js

--- mockData.js
+++ mockData.js
@@ -1,15 +1,18 @@
-const data = {
+
+
+
+const data = {     
+  
   "sell_info": {
     "cart": [
       {
-        
         "product_details": {
           "product_id": "6568539997b0da5e821c4019",
           "po_product_id": "65a08e43f3ff48b82205b902",
           "Metal": "6355fbddc734e545a850dd02",
           "Stones": [
             {
-              "stone_group": "63560539dcf7ec0c616fea1f",
+              "stone_group": "63560539dcf7ec0c616fea1fa",
               "stone": "6356061d2572146aa404d2d2",
               "Variant": "2",
               "Align": "",
@@ -28,18 +31,19 @@ const data = {
               "certificate": {
                 "type": "",
           
-                "number": ""
+                "number": "3750983905830985"
               },
-              "size_name": "3.80MM",
-              "stone_name": "Diamond",
-              "shape_name": "Brilliant Round",
-              "clarity_name": "VS2",
-              "color_name": "J",
-              "setting_name": "Solitaire",
-              "cut_name": "Heart & Arrow"
+              "size_name": "1 Ruby 5c",
+              "stone_name": "18 Karat Gold Diamond Crown Ring",
+              "shape_name": "Brilliant Round1",
+              "clarity_name": "26 Dia 00.9c",
+              "color_name": "18K 3.90g",
+              "setting_name": "Intricately crafted crown design",
+              "cut_name": "Heart & Arrow1"
             },
+            
             {
-              "stone_group": "63560539dcf7ec0c616fea1f",
+              "stone_group": "63560539dcf7ec0c616fea1fb",
               "stone": "6356061d2572146aa404d2d2",
               "Variant": "0",
               "Align": "",
@@ -48,7 +52,7 @@ const data = {
               "quality": "",
               "clarity": "636c7b71671a877a9c779c81",
               "color": "636c7c4a28021528d413c0b9",
-              "Size": "63560917044ba54bde4d2a16",
+              "Size": "63560917044ba54bde4d2a16b",
               "Unit": "Cts",
               "Pcs": "100",
               "Cts": "0.20",
@@ -59,16 +63,18 @@ const data = {
                 "type": "",
                 "number": ""
               },
+
               "size_name": "3.80MM",
-              "stone_name": "Diamond",
-              "shape_name": "Brilliant Round",
-              "clarity_name": "VS2",
-              "color_name": "J",
-              "setting_name": "Pave",
+              "stone_name": "Diamond2",
+              "shape_name": "Brilliant Round2",
+              "clarity_name": "VS22",
+              "color_name": "J2Diamond",
+              "setting_name": "Pave2",
               "cut_name": "Normal Cut"
             },
+
             {
-              "stone_group": "63560539dcf7ec0c616fea1f",
+              "stone_group": "18 Karat Gold Diamond Crown Ring",
               "stone": "6356061d2572146aa404d2d2",
               "Variant": "0",
               "Align": "",
@@ -77,30 +83,35 @@ const data = {
               "quality": "",
               "clarity": "636c7b8928021528d413c0b7",
               "color": "636c7c4a28021528d413c0b9",
-              "Size": "63858e2ec0d74d7f332eaa15",
+              "Size": "63858e2ec0d74d7f332eaa156",
               "Unit": "Cts",
               "Pcs": "2",
               "Cts": "0.20",
-              "Price": "0.00",
+              "Price": "$10000",
               "Pointer": "0",
               "setting_type": "635648d4c0d74d7f332ea994",
               "certificate": {
                 "type": "",
                 "number": ""
               },
-              "size_name": "1.25MM",
-              "stone_name": "Diamond",
-              "shape_name": "Brilliant Round",
-              "clarity_name": "VVS2",
-              "color_name": "J",
-              "setting_name": "Pave",
+              "size_name": "4.80MM",
+              "stone_name": "Intricately crafted crown design",
+              "shape_name": "Brilliant Round3",
+              "clarity_name": "VS223",
+              "color_name": "JDiamond3",
+              "setting_name": "Pointer: 0.2044",
               "cut_name": "Normal Cut"
             }
           ],
+
           "product_variants_enable": "2",
           "stock_id": "2211761",
           "po_QTY": 1,
-          "retailprice_Inc": 31800
+          "retailprice_Inc": 318000
+        },
+        "customer_contact": {
+          "mobile_number": "+1-555-1234",
+          "email": "scarlett.johansson@example.com"
         },
         "customer_id": "6379ae40a14336b22805540c",
         "sales_person_id": "6379ae9e28021528d413c0df",
@@ -148,16 +159,16 @@ const data = {
               "type": "",
               "number": ""
             },
-            "size_name": "3.80MM",
-            "stone_name": "Diamond",
-            "shape_name": "Brilliant Round",
-            "clarity_name": "VS2",
-            "color_name": "J",
+            "size_name": "3.80MMD",
+            "stone_name": "26 Ruby 9c",
+            "shape_name": "Brilliant Round4",
+            "clarity_name": "187K 3.90g",
+            "color_name": "Service Ring Engrave",
             "setting_name": "Solitaire",
             "cut_name": "Heart & Arrow"
           },
           {
-            "stone_group": "63560539dcf7ec0c616fea1f",
+            "stone_group": "63560539dcf7ec0c616fea1fb5",
             "stone": "6356061d2572146aa404d2d2",
             "Variant": "0",
             "Align": "",
@@ -177,12 +188,12 @@ const data = {
               "type": "",
               "number": ""
             },
-            "size_name": "3.80MM",
-            "stone_name": "Diamond",
-            "shape_name": "Brilliant Round",
-            "clarity_name": "VS2",
-            "color_name": "J",
-            "setting_name": "Pave",
+            "size_name": "3.80MM90",
+            "stone_name": "23 Ruby 9c",
+            "shape_name": "Brilliant Round5",
+            "clarity_name": "187K 3.907g",
+            "color_name": "Service Ring Engrav1",
+            "setting_name": "Solitaire diamond",
             "cut_name": "Normal Cut"
           },
           {
@@ -207,8 +218,8 @@ const data = {
               "number": ""
             },
             "size_name": "1.25MM",
-            "stone_name": "Diamond",
-            "shape_name": "Brilliant Round",
+            "stone_name": "1.01 carats, G colour, SI1 clarity",
+            "shape_name": "round ideal Cut",
             "clarity_name": "VVS2",
             "color_name": "J",
             "setting_name": "Pave",
@@ -251,6 +262,8 @@ const data = {
         "id": "6568539997b0da5e821c4019"
       }
     ],
+
+
     "deletecart_ids": [],
     "summary_order": {
       "labour": 0,
@@ -273,7 +286,7 @@ const data = {
       "tax_amount_int": 2081,
       "tax_prect": 7,
       "tax_type": "Inclusive",
-      "Sub_Total": 29719
+      "Sub_Total": 9719
     },
     "status_type": "pos_order",
     "totalamount": 31800,
@@ -354,4 +367,4 @@ const data = {
     }
   }
 };
-
+module.exports = { data };
